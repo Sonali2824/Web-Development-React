291ba40bbe38a567f60e6b9d810ee221f7057854
Sonali Nandagopalan <57223104+Sonali2824@users.noreply.github.com>
2022-01-17 18:24:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render("what to show","where to show");
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
